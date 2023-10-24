ae03e776f885b45a39106374bf5f4f885099fe7e
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-10-23 23:07:52 -0500

    added components and a start for the navbar and routes to be used for the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+// import { useState } from 'react';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+import Nav from "./components/Nav"
+// import Home from "./components/Home"
+// import Header from "./components/Header"
+// import About from "./components/About"
+// import Project from "./components/Project"
+// import Contact from "./components/Contact"
+// import Footer from "./components/Footer"
 import './App.css'
 
-function App() {
-
+export default function App() {
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <Router>
+      <Nav />
+      {/* <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/projects'>
+          <Project />
+        </Route>
+        <Route path='/contact'> 
+        </Route>
+      </Switch> */}
+    </Router>
   )
 }
-
-export default App
